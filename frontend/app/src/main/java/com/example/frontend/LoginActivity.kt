4fe3ac38cd82db4f49a3a2ed6ed7eb24fb0c9664
Shyanue <Shyanue@gmail.com>
2021-10-13 19:22:00 +0900

    [feat/front] 로딩 - 로그인 - 회원가입 간 화면 이동 기능 추가 - #5
    body : 로딩 - 로그인 - 회원가입 간 화면 이동 기능 추가, 로딩 화면 컨텐츠 중앙에 배치되도록 조정

--- frontend/app/src/main/java/com/example/frontend/LoginActivity.kt
+++ frontend/app/src/main/java/com/example/frontend/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.example.frontend
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnJoin = findViewById<Button>(R.id.joinButton)
+        btnJoin.setOnClickListener(){
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
